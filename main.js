2cc1b0a56847622a372a97a414896c4b5e9fa566
poerevan9 <poerevan9@gmail.com>
2023-02-16 01:44:41 +0800

    services & why

---
+++ main.js
@@ -0,0 +1,39 @@
+// set carousel interval
+const myCarouselElement = document.querySelector("#heroCarousel");
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  touch: false,
+});
+
+// stop navbar transition when modal is open
+var myModalEl = document.getElementById("meetHypnModal");
+var modalInShownState = false;
+myModalEl.addEventListener("shown.bs.modal", function (event) {
+  modalInShownState = true;
+});
+myModalEl.addEventListener("hidden.bs.modal", function (event) {
+  modalInShownState = false;
+});
+
+$(window).on("wheel", function (event) {
+  // control nav transition
+  if (modalInShownState == false) {
+    if (event.originalEvent.deltaY < 0 && scrollY < innerHeight) {
+      // wheeled up
+      $(".nav-link").css("font-size", "1.75rem");
+      $(".navbar").addClass("my-3");
+    } else {
+      // wheeled down
+      $(".nav-link").css("font-size", "1.5rem");
+      $(".navbar").removeClass("my-3");
+    }
+  }
+  // control img transition
+  if (scrollY > $(".more-section").offset().top - innerHeight + 300) {
+    $("#scroll-img").addClass("shrink");
+    $("#scroll-img").removeClass("grow");
+  } else {
+    $("#scroll-img").removeClass("shrink");
+    $("#scroll-img").addClass("grow");
+  }
+});
