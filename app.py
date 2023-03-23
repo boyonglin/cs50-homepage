79b54b6d341bf4dfcccb3ae31be89efc3bcf851e
Clancy Lin <poerevan9@gmail.com>
2023-03-24 01:42:41 +0800

    add new routes

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ TIMES = [
 def index():
     return render_template("index.html", times=TIMES)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/reservation")
+def reservation():
+    return render_template("reservation.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
